0da7fb9d40d59e38a31b6f07778431250aa7b002
prameela <prameelanamala.com>
2024-08-28 21:13:57 +0530

    installing mysql

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LOGS_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/expense-shell"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
